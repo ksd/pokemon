52ed2557343b12cd93f8b206338826ce32b128a6
Kaj Didriksen <kdidriksen@me.com>
2021-10-15 12:05:01 +0200

    Endelig Final lektion 07

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:pokemon/pokemon_detail.dart';
+import 'package:pokemon/pokemon_controller.dart';
+//import 'package:pokemon/pokemon_detail.dart';
+import 'package:provider/provider.dart';
 import 'pokemon.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+        create: (BuildContext context) => PokemonController(),
+        child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,51 +28,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-Future<List<Pokemon>> fetchPokemons() async {
-  final response =
-      await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/?limit=50'));
-  List<Pokemon> pokemons = [];
-  if (response.statusCode == 200) {
-    var jsonData = json.decode(response.body);
-
-    List<dynamic> jsonPokemons = jsonData['results'];
-    for (int i = 0; i < jsonPokemons.length; i++) {
-      var pokemon = jsonPokemons[i];
-      pokemons.add(Pokemon.fromJSON(pokemon));
-    }
-  }
-  return pokemons;
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late Future<List<Pokemon>> _futurePokemonList;
-
-  @override
-  void initState() {
-    super.initState();
-    _futurePokemonList = fetchPokemons();
-  }
-
-  _navigate_to_Detail(Pokemon pokemon, int index) async {
-    var editedPokemon = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) {
-          return PokemonDetail(
-            pokemon: pokemon,
-          );
-        },
-      ),
-    );
-    
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,41 +37,23 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Pokemons"),
       ),
-      body: FutureBuilder(
-        future: _futurePokemonList,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List<Pokemon> pokemonsSnapshot = snapshot.data as List<Pokemon>;
-            return ListView.builder(
-              itemCount: pokemonsSnapshot.length,
-              itemBuilder: (context, index) {
-                Pokemon pokemon = pokemonsSnapshot[index];
-                return InkWell(
-                  onTap: () => {_navigate_to_Detail(pokemon, index)},
-                  child: Card(
-                      child: ListTile(
-                    title: Text(pokemon.name),
-                    leading: FutureBuilder(
-                        future: pokemon.fetchImageURL(),
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            String imageURL = snapshot.data as String;
-                            return Image.network(imageURL);
-                          } else {
-                            return const CircularProgressIndicator();
-                          }
-                        }),
-                  )),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text("$snapshot.error");
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
-      ),
+      body: Consumer<PokemonController>(
+          builder: (context, _pokemonController, widget) {
+        return ListView.builder(
+            itemCount: _pokemonController.numberOfPokemons,
+            itemBuilder: (context, index) {
+              final Pokemon pokemon =
+                  _pokemonController.getPokemonOf(index: index);
+              return pokemonRow(context, pokemon);
+            });
+      }),
+    );
+  }
+
+  pokemonRow(BuildContext context, Pokemon pokemon) {
+    return ListTile(
+      leading: const Icon(Icons.person_pin_outlined),
+      title: Text(pokemon.name),
     );
   }
 }
